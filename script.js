084d96b8a6427dfab61689d7503fd5e3d8b823b8
He Lingzheng <lhe76@wisc.edu>
2020-09-13 11:14:18 -0500

    added fairmoji changes; will redirect user to meeting room

--- script.js
+++ script.js
@@ -1,77 +1,141 @@
 var selections = [];
+var questionNumber = 0;
 
-$(document).ready(function() {
-    start(questionNumber);
-    
-    $(".submit-answer").on("click", function(event) {
-    
-        var userAnswer = parseInt($(this).attr("id"));
-        selections.push(userAnswer);
-        $("#" + userAnswer).addClass("clickStyle");
-        setTimeout(function() {
-                    $(".submit-answer").removeClass("clickStyle");
-                     start(questionNumber);
-                 }, 1000)
-    
-         questionNumber++;
-      });
-    
-    });
-    
-    var questionNumber = 0,
-        optionFinal = 0;
-    
-    var allQuestions = [
-         {
-            question: 'What kind of food do you like?',
-            choices: ["Hamburger", "Ice cream", "Pizza","Salad"],}
-        ,{
-            question: "Which year will you graduate?",
-            choices: ["2020", "2021", "2022", "2023"],}
-        ,{
-            question: "Which one is your dream pet?",
-            choices: ["Unicorn", "Sphinx", "Spider", "Crocodile"],}
-        ,{
-            question: 'Which one is the most relaxing place in the world?',
-            choices: ["Beach", "Moon", "Mountain", "Internet"],}
-      ];
-    
-    // continue with next question or end
-    var start = function(questionNumber) {
-          $('h2').hide().fadeIn(400);
-    
-          if(questionNumber !== allQuestions.length){
-              question(questionNumber);
-          }else{
-              end();
-          }
-    };
-    
-    // show question and possible answers
-    function question(questionNum) {
-          $("h2").text(allQuestions[questionNum].question);
-    
-          $.each(allQuestions[questionNum].choices, function(i, answers){
-             $("#" + i).html(answers);
-          });
-    };
-    
-    function end() {
-      finalImage();
-      $("ul").hide();
-      $("h2").text("Here is your result!");
-      $("#image").html('<img src=' + "" + ' alt="">').fadeIn(1000);
-      restart();
-    };
-    
-    // TODO
-    function finalImage() {
-      if (selections[0] = 1){
+$(document).ready(function () {
+  start(questionNumber);
 
-      }
-    }
-    
-    
-    
-    
-    
+  $(".submit-answer").on("click", function (event) {
+    var userAnswer = parseInt($(this).attr("id"));
+    selections.push(userAnswer);
+    $("#" + userAnswer).addClass("clickStyle");
+    setTimeout(function () {
+      $(".submit-answer").removeClass("clickStyle");
+      start(questionNumber);
+    }, 250);
+
+    questionNumber++;
+  });
+});
+
+var images = [
+  {
+    paths: [
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/relieved-face_1f60c.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/smiling-face-with-sunglasses_1f60e.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/grinning-face-with-star-eyes_1f929.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/face-with-uneven-eyes-and-wavy-mouth_1f974.png"
+    ]
+  },
+  {
+    paths: [
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/party-popper_1f389.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/school-satchel_1f392.png"
+    ]
+  },
+  {
+    paths: [
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/llama_1f999.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/sloth_1f9a5.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/turtle_1f422.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/crocodile_1f40a.png"
+    ]
+  },
+  {
+    paths: [
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/hamburger_1f354.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/ice-cream_1f368.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/burrito_1f32f.png",
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/green-salad_1f957.png"
+    ]
+  }
+];
+
+var resultimg = [
+  "http://drive.google.com/thumbnail?id=1mx31yg84BAmwdflh7FmFR8s1cfS1bbDo",
+  "http://drive.google.com/thumbnail?id=1eUpqEGpKg-0VIyuk0XKVM2xhrk6PTPv6",
+  "http://drive.google.com/thumbnail?id=1PsEZgQPYu_Jh-4QYABM1unIQ1jt29unu",
+  "http://drive.google.com/thumbnail?id=1sy3gPn34Z-gVAWHvXa_ecIBiupfWKJaL"
+];
+
+var allQuestions = [
+  {
+    question: "How are you feeling today?",
+    choices: ["Calm.", "Cool-", "Excited!", "Kinda Tired"]
+  },
+  {
+    question: "Are you graduating this year?",
+    choices: ["Yes!", "Not yet!"]
+  },
+  {
+    question: "Which one is your dream pet?",
+    choices: ["Llama", "Sloth", "Turtle", "Crocodile"]
+  },
+  {
+    question: "Which one is your favoraite food?",
+    choices: ["Hamburger", "Ice cream", "Burrito", "Salad"]
+  }
+];
+
+function showfairmoji(questionNum) {
+  document.getElementById("fairtext").style.visibility = "visible";
+  if (questionNum !== 0) {
+    document.getElementById("fairmoji").style.visibility = "visible";
+    document.getElementById("fairmoji").src = resultimg[questionNum - 1];
+  }
+}
+
+// continue with next question or end
+var start = function (questionNumber) {
+  $("h2").hide().fadeIn(400);
+  if (questionNumber !== allQuestions.length) {
+    question(questionNumber);
+  } else {
+    end();
+  }
+};
+
+// show question and possible answers with images
+function question(questionNum) {
+  $("h2").text(allQuestions[questionNum].question);
+  if (questionNumber == 1) {
+    document.getElementById("2").style.visibility = "hidden";
+    document.getElementById("3").style.visibility = "hidden";
+    document.getElementById("img2").style.visibility = "hidden";
+    document.getElementById("img3").style.visibility = "hidden";
+  } else {
+    document.getElementById("2").style.visibility = "visible";
+    document.getElementById("3").style.visibility = "visible";
+    document.getElementById("img2").style.visibility = "visible";
+    document.getElementById("img3").style.visibility = "visible";
+  }
+
+  $.each(allQuestions[questionNum].choices, function (i, answers) {
+    $("#" + i).html(answers);
+  });
+  $.each(images[questionNum].paths, function (j, path) {
+    document.getElementById("img" + j + "").src = path;
+  });
+  showfairmoji(questionNum);
+}
+
+function end() {
+  document.getElementById("fairtext").style.visibility = "hidden";
+  $("ul").hide();
+  $("h2").text("Here is your result!");
+  document.getElementById("fairmoji").src = resultimg[3];
+
+  setTimeout(redirect, 3000);
+}
+
+function redirect() {
+  if (
+    window.confirm(
+      "You are the next one in line to speak with John from Credera. Click OK to be redirected to the meeting room!"
+    )
+  ) {
+    window.location =
+      "https://teams.microsoft.com/dl/launcher/launcher.html?type=meetup-join&deeplinkId=05bc4c19-6ede-485c-96b7-91a448037aac&directDl=true&msLaunch=true&enableMobilePage=true&url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_NTcwODY2NDgtNDljMy00M2E3LTkwNmYtY2NmYjc2Yzg3MDMx@thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522780a38ce-43be-4c57-ad79-22391ed2d333%2522%252c%2522Oid%2522%253a%25227febfae8-bc72-4f30-8fa6-34dec5dcba86%2522%257d%26anon%3Dtrue&suppressPrompt=true";
+  } else {
+    alert("You exited the line!");
+  }
+}
